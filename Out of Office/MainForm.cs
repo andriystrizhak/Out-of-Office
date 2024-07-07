d0d1a44b989a388e4b5061ec8d2f1fc1397e7c2b
Andriy Strizhak <andriystrizhakpetrov@gmail.com>
2024-07-07 16:17:18 +0300

    Add project files.

---
+++ Out of Office/MainForm.cs
@@ -0,0 +1,12 @@
+namespace Out_of_Office
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //TODO - Add Methods
+    }
+}
